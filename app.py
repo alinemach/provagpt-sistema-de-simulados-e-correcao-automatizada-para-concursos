352d5b883fdefea71f36a05dba8e6f9b7ab84b84
alinemach <alinemach@gmail.com>
2024-08-23 19:29:01 -0300

    rota de delete adicionada

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.upload import upload_file
+from routes.delete import delete_file
 
 app = Flask(__name__)
 CORS(app)
 
 # Registrando rotas
 app.register_blueprint(upload_file, url_prefix='/api')
+app.register_blueprint(delete_file, url_prefix='/api')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
