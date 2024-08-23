70e5cbcb7a2793b40d239e863e4f7ad9f5d8d1cf
alinemach <alinemach@gmail.com>
2024-08-23 18:26:33 -0300

    logica de processamentos de pdf e imagens adicionado

---
+++ services/process.py
@@ -0,0 +1,16 @@
+from PyPDF2 import PdfReader
+from PIL import Image
+import pytesseract
+
+def extract_text_from_pdf(file_path):
+    text = ""
+    with open(file_path, "rb") as file:
+        reader = PdfReader(file)
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
+
+def extract_text_from_image(file_path):
+    image = Image.open(file_path)
+    text = pytesseract.image_to_string(image)
+    return text
